e8927aabed84e89cbf7c44185c96c0e60c4cdc12
Berkant Cen <berkantyazilim@gmail.com>
2021-10-28 20:22:56 +0300

    fix the bug causing quiz to crash

--- dice_rolling_game/app.js
+++ dice_rolling_game/app.js
@@ -1,88 +1,76 @@
-// GAME LOGIC 
+// GAME LOGIC
 
-const firstDice = document.getElementById("first")
-const secondDice = document.getElementById("second")
-const heading = document.querySelector("h1")
-const btn = document.getElementById("btn")
+const firstDice = document.getElementById('first');
+const secondDice = document.getElementById('second');
+const heading = document.querySelector('h1');
+const btn = document.getElementById('btn');
 
+let firstPlayerName = 'Player ONE';
+let secondPlayerName = 'Player TWO';
 
-let firstPlayerName = "Player ONE"
-let secondPlayerName = "Player TWO"
+const firstPlayer = document.querySelector('.first-player');
+const secondPlayer = document.querySelector('.second-player');
+const startingNameOne = document.getElementById('starting-name-one');
+const startingNameTwo = document.getElementById('starting-name-two');
+const nameBtnOne = document.querySelector('.player-name-btn');
+const nameBtnTwo = document.querySelector('.second-player button');
 
-const firstPlayer = document.querySelector(".first-player")
-const secondPlayer = document.querySelector(".second-player")
-const startingNameOne = document.getElementById("starting-name-one")
-const startingNameTwo = document.getElementById("starting-name-two")
-const nameBtnOne = document.querySelector(".player-name-btn")
-const nameBtnTwo = document.querySelector(".second-player button")
-
-function nameSaved(){
-    Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Your name has been set',
-        showConfirmButton: false,
-        timer: 1500
-      })
+function nameSaved() {
+  Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Your name has been set',
+    showConfirmButton: false,
+    timer: 1500,
+  });
 }
 
-function nameEmpty(){
-    Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'You didnt write any name',
-        
-      })
+function nameEmpty() {
+  Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: 'You didnt write any name',
+  });
 }
 
+nameBtnOne.addEventListener('click', () => {
+  firstPlayerName = document.getElementById('first-player-name').value;
+  if (firstPlayerName === '') {
+    nameEmpty();
+  } else {
+    startingNameOne.innerText = firstPlayerName;
+    firstPlayer.remove();
+    nameSaved();
+  }
+});
+nameBtnTwo.addEventListener('click', () => {
+  secondPlayerName = document.getElementById('second-player-name').value;
+  if (secondPlayerName === '') {
+    nameEmpty();
+  } else {
+    startingNameTwo.innerText = secondPlayerName;
+    secondPlayer.remove();
+    nameSaved();
+  }
+});
 
-nameBtnOne.addEventListener("click",()=> {
-    firstPlayerName = document.getElementById("first-player-name").value
-    if(firstPlayerName ===""){
-        nameEmpty()
-    }
-    else{
-        startingNameOne.innerText = firstPlayerName
-        firstPlayer.remove()
-        nameSaved()
-    }
+btn.addEventListener('click', rollDices);
 
-})
-nameBtnTwo.addEventListener("click",()=> {
-    secondPlayerName = document.getElementById("second-player-name").value
-if(secondPlayerName === ""){
-    nameEmpty()
+function rollDices() {
+  let firstRoll = Math.floor(Math.random() * 6 + 1);
+  let secondRoll = Math.floor(Math.random() * 6 + 1);
+
+  while (firstRoll === secondRoll) {
+    secondRoll = Math.floor(Math.random() * 6 + 1);
+  }
+
+  if (firstRoll > secondRoll) {
+    heading.innerText = `${firstPlayerName} crushed ${secondPlayerName}`;
+  } else {
+    heading.innerText = `${secondPlayerName} showed no mercy to  ${firstPlayerName}`;
+  }
+  firstDice.setAttribute('src', `img/dice${firstRoll}.png`);
+  secondDice.setAttribute('src', `img/dice${secondRoll}.png`);
 }
-    else{
-        startingNameTwo.innerText = secondPlayerName
-        secondPlayer.remove()
-        nameSaved()
-    }
-
-})
-
-
-btn.addEventListener("click", rollDices);
-
-
-function rollDices(){
-    let firstRoll = Math.floor(Math.random() * 6 + 1);
-    let secondRoll = Math.floor(Math.random() * 6 +1 );
-    
-    while(firstRoll === secondRoll){
-        secondRoll = Math.floor(Math.random() * 6 +1 );
-    }
-
-    if(firstRoll > secondRoll){
-        heading.innerText =  `${firstPlayerName} crushed ${secondPlayerName}`
-    }else{
-        heading.innerText = `${secondPlayerName} showed no mercy to  ${firstPlayerName}`
-    }                 
-    firstDice.setAttribute("src", `/img/dice${firstRoll}.png`)
-    secondDice.setAttribute("src", `/img/dice${secondRoll}.png`)
-
-}
-
-
 
 // SKOR,SOUND EFFECT,ROTATION ANIMATION,OYUNCU SAYISI SEÇME,LABEL TIKLAYIP ISIM ,WİNNER
--- quiz/app.js
+++ quiz/app.js
@@ -1,132 +1,122 @@
+// QUESTIONS
 const questions = [
   {
-    question: "Which of the following is used for setting text color? ",
-    answer1: "text-color",
-    answer2: "background-color",
-    correctAnswer: "color",
+    question: 'Which of the following is used for setting text color? ',
+    answer1: 'text-color',
+    answer2: 'background-color',
+    correctAnswer: 'color',
   },
   {
     question: 'What is the usage of "font-weight"?',
-    answer1: "Adjusting font color",
-    answer2: "Adjusting size of the font",
-    correctAnswer: "Adjusting how bold text will be",
+    answer1: 'Adjusting font color',
+    answer2: 'Adjusting size of the font',
+    correctAnswer: 'Adjusting how bold text will be',
   },
 
   {
-    question: "Which of the following doesnt give an error?",
-    answer1: "font-size = 20px",
-    answer2: "background-color : 2rem",
-    correctAnswer: "display : flex;",
+    question: 'Which of the following doesnt give an error?',
+    answer1: 'font-size = 20px',
+    answer2: 'background-color : 2rem',
+    correctAnswer: 'display : flex;',
   },
 
   {
-    question: "What is Css stands for ? ",
-    answer1: "Current style state",
-    answer2: "Cascading super style",
-    correctAnswer: "Cascading style sheet",
+    question: 'What is Css stands for ? ',
+    answer1: 'Current style state',
+    answer2: 'Cascading super style',
+    correctAnswer: 'Cascading style sheet',
   },
 
   {
-    question: "Which of the following is Pre-compiler of Css",
-    answer1: "React",
-    answer2: "Vue",
-    correctAnswer: "Sass",
+    question: 'Which of the following is Pre-compiler of Css',
+    answer1: 'React',
+    answer2: 'Vue',
+    correctAnswer: 'Sass',
   },
 ];
 
-let score = 0;
-let i = 1;
-//DOM ELEMENTS
-const startBtn = document.querySelector(".start-btn");
-const container = document.querySelector(".container");
+// DOM ELEMENTS
+const startScreen = document.querySelector('.start-screen');
+const startBtn = document.querySelector('.start-btn');
+const questionContainer = document.querySelector('.question-container');
+const answers = document.querySelectorAll('.answer');
+const answerList = document.querySelector('.answer-list');
+const nextBtn = document.querySelector('.next-btn');
 
-// STARTING QUESTIONS
-startBtn.addEventListener("click", function () {
-  container.innerHTML = `
-  <h3 class="question">
-  Q1 : ${questions[i - 1].question}
-</h3>
-<ul class="answer-list">
-  <li class="answer" onclick="selectGuess(event)">${
-    questions[i - 1].answer1
-  }</li>
-  <li class="answer" onclick="selectGuess(event)">${
-    questions[i - 1].correctAnswer
-  }</li>
-  <li class="answer" onclick="selectGuess(event)" = >${
-    questions[i - 1].answer2
-  }r</li>
-</ul>
-<button class="btn next-btn">Next</button>
-`;
+const question = document.querySelector('h3');
+const answer1 = document.querySelector('.answer-1');
+const answer2 = document.querySelector('.answer-2');
+const answer3 = document.querySelector('.answer-3');
+
+// GLOBAL VARIABLES
+let questionCounter = 0;
+let score = 0;
+let guess;
+
+// STARTING THE QUIZ
+startBtn.addEventListener('click', function () {
+  questionContainer.style.display = 'flex';
+  startScreen.style.display = 'none';
 });
 
-function selectGuess(event) {
-  const answers = document.querySelectorAll(".answer");
+// SELECTING ANSWERS
+answerList.addEventListener('click', function (e) {
   answers.forEach((answer) => {
-    answer.classList.remove("active");
+    answer.classList.remove('active');
   });
-  event.target.classList.toggle("active");
-  let guess = event.target.textContent;
 
-  const nextBtn = document.querySelector(".next-btn");
+  if (e.target.classList.contains('answer')) {
+    e.target.classList.add('active');
+    guess = e.target.textContent;
+    console.log(guess);
+  }
+});
 
-  nextBtn.addEventListener("click", function () {
-    if (guess === questions[i - 1].correctAnswer) {
-      score++;
-      console.log("if", score);
-    }
+// CHECKING THE ANSWER AND NEXT QUESTION
 
-    if (questions[i] === undefined) {
-      container.innerHTML = `You were correct at ${score} of ${questions.length}`;
-      return;
-    }
-    container.innerHTML = correctAnswerTemplate();
+nextBtn.addEventListener('click', function () {
+  if (questions[questionCounter].correctAnswer == guess) {
+    score++;
+  }
+  if (questionCounter === 4) {
+    return (questionContainer.innerHTML = `Your score is ${score}`);
+  }
+  questionCounter++;
 
-    i++;
+  answers.forEach((answer) => {
+    answer.classList.remove('active');
   });
-}
+  question.textContent = `Q${questionCounter + 1}: ${
+    questions[questionCounter].question
+  }`;
+  let order = randomizeQuestion();
+  console.log(order);
+  answer1.textContent = order[0];
+  answer2.textContent = order[1];
+  answer3.textContent = order[2];
+});
 
-function correctAnswerTemplate() {
-  let answersTemplate = [
-    `
-  <h3 class="question">
-  Q1 : ${questions[i].question}
-</h3>
-<ul class="answer-list">
-  <li class="answer" onclick="selectGuess(event)">${questions[i].answer1}</li>
-  <li class="answer" onclick="selectGuess(event)">${questions[i].answer2}</li>
-  <li class="answer" onclick="selectGuess(event)">${questions[i].correctAnswer}</li>
-</ul>
-<button class="btn next-btn">Next</button>
-`,
+// RANDOM QUESTION ORDER
 
-    `
-  <h3 class="question">
-  Q1 : ${questions[i].question}
-</h3>
-<ul class="answer-list">
-  <li class="answer" onclick="selectGuess(event)">${questions[i].correctAnswer}</li>
-  <li class="answer" onclick="selectGuess(event)">${questions[i].answer2}</li>
-  <li class="answer" onclick="selectGuess(event)">${questions[i].answer1}</li>
-</ul>
-<button class="btn next-btn">Next</button>
-`,
-
-    `
-  <h3 class="question">
-  Q1 : ${questions[i].question}
-</h3>
-<ul class="answer-list">
-  <li class="answer" onclick="selectGuess(event)">${questions[i].answer2}</li>
-  <li class="answer" onclick="selectGuess(event)">${questions[i].correctAnswer}</li>
-  <li class="answer" onclick="selectGuess(event)">${questions[i].answer1}</li>
-</ul>
-<button class="btn next-btn">Next</button>
-`,
+function randomizeQuestion() {
+  let questionOrder = [
+    [
+      questions[questionCounter].answer1,
+      questions[questionCounter].answer2,
+      questions[questionCounter].correctAnswer,
+    ],
+    [
+      questions[questionCounter].correctAnswer,
+      questions[questionCounter].answer1,
+      questions[questionCounter].answer2,
+    ],
+    [
+      questions[questionCounter].answer2,
+      questions[questionCounter].correctAnswer,
+      questions[questionCounter].answer1,
+    ],
   ];
 
-  let templateRandom = Math.floor(Math.random() * answersTemplate.length);
-
-  return answersTemplate[templateRandom];
+  let selectedOrder = Math.floor(Math.random() * 3);
+  return questionOrder[selectedOrder];
 }
